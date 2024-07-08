6568bcafacc3c03c60b0b37e08a5f0d9544f539f
Azeezat Omobolanle <155020971+simplicityf@users.noreply.github.com>
2024-07-08 18:18:36 +0100

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,29 @@
+//variable
+
+let thumb= document.getElementsByClassName("thumb");
+let slider = document.getElementById("slider");
+
+const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+ // document.alert(maxScrollLeft);
+
+ //autolay slider
+
+ function autoPlay() {
+    if (slider.scrollLeft > (maxScrollLeft - 1)) {
+        slider.scrollLeft -= maxScrollLeft;
+    } else {
+        slider.scrollLeft += 1;
+    }
+ }
+
+ let play = setInterval(autoPlay, 10);
+
+ //pause slider on hover
+ for(let i=0; i<thumb.length; i++){
+    thumb[i].addEventListener("mouseover", () =>{
+        clearInterval(play)
+    })
+    thumb[i].addEventListener("mouseout", () =>{
+        return play = setInterval(autoPlay, 10);
+    })
+ }
